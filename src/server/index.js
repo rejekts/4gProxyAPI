189c5b41d62c6f87817f48a62e7768f67c075764
Justin <justscribner@gmail.com>
2018-10-03 16:08:10 -0500

    working on IP showing in browser

--- src/server/index.js
+++ src/server/index.js
@@ -38,7 +38,7 @@ app.get("/proxy1/reset", function(req, res) {
 
   shell.exec(path.join(__dirname + "/pi1Reset.sh"));
 
-  res.redirect("http://proxy.confucius.marketing");
+  res.redirect("https://ipfingerprints.com");
 });
 //sudo nmcli device disconnect cdc-wdm0 && sudo nmcli device connect cdc-wdm0
 //Using node-ssh wrapper below
