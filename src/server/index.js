dfcad5310ed6fb81bc8792c80a7d9ca4839746f2
Justin <justscribner@gmail.com>
2018-10-29 12:06:07 -0500

    working on reset functionality

--- src/server/index.js
+++ src/server/index.js
@@ -17,49 +17,20 @@ app.use(express.static("dist"));
 // app.use(requestIp.mw());
 app.enable("trust proxy");
 
-app.get("/api/getUsername", (req, res) =>
-  res.send({ username: os.userInfo().username })
-);
 const server = app.listen(8080, () => console.log("Listening on port 8080!"));
 
-//Using simple-ssh wrapper below
-//Original UUID - e101f2c1-0dcd-4ce8-8744-c198faa828d7
-
-app.get("/proxy1", function(req, res) {
-  console.log("Redirect to Main IP Page API Endpoint getting hit!", req.ip);
-  let iplib = req.clientIp;
-  console.log("request-ip lib IP => ", iplib);
-  var ip =
-    req.headers["x-forwarded-for"] ||
-    req.connection.remoteAddress ||
-    req.socket.remoteAddress ||
-    (req.connection.socket ? req.connection.socket.remoteAddress : null);
-
-  res.send(`Hello. Your new IP Address is ${ip}`);
-});
-
 app.get("/proxy/reset", function(req, res) {
-  console.log("Reset API Endpoint getting hit!");
-  const host = req.param.host;
-  const network = req.param.network;
-  console.log("host ip => ", host, " network => ", network);
+  const host = req.param("host");
+  const network = req.param("network");
+  console.log(
+    "Reset API Endpoint getting hit!",
+    "host ip => ",
+    host,
+    " network => ",
+    network
+  );
 
-  // var interfaces = os.networkInterfaces();
-  // console.log("Interaces => ", interfaces);
-  // var addresses = [];
-  // for (var k in interfaces) {
-  //   for (var k2 in interfaces[k]) {
-  //     var address = interfaces[k][k2];
-  //     if (address.family === "IPv4" && !address.internal) {
-  //       addresses.push(address.address);
-  //     }
-  //   }
-  // }
-
-  // console.log("Addresses => ", addresses);
-  //path.join(__dirname + "/pi1ResetVerizon.sh")
-
-  child_process.execFile(
+  child_process.exec(
     `ssh pi@${host} "sudo nmcli connection up ${network}"`,
     (error, stdout) => {
       if (error) {
@@ -67,23 +38,25 @@ app.get("/proxy/reset", function(req, res) {
         return;
       } else {
         console.log(`Connection: ${stdout}`);
+        res.redirect(`http://ipfingerprints.com`);
       }
     }
   );
-
-  res.send(`Hello. Your new IP Address is ${ip}`);
 });
-//sudo nmcli device disconnect cdc-wdm0 && sudo nmcli device connect cdc-wdm0
-//Using node-ssh wrapper below
 
-app.get("/proxy1/reset/hard", function(req, res) {
-  console.log("Hard Reset API Endpoint getting hit!");
+app.get("/proxy/reset/hard", function(req, res) {
+  const host = req.param("host");
+  console.log("Hard Reset API Endpoint getting hit!", "host => ", host);
 
-  // shell.exec(
-  //   "/home/justin/Dropbox/+++Coding+++/code/Confucius/Proxies/4gProxyAPI/src/server/pi1HardReset.sh"
-  // );
-
-  shell.exec(path.join(__dirname + "/pi1HardReset.sh"));
-
-  res.sendFile(path.join(__dirname + "/resetIndex.html"));
+  child_process.exec(`ssh pi@${host} "sudo reboot"`, (error, stdout) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    } else {
+      console.log(`Connection: ${stdout}`);
+      res.send(
+        `Proxy Server Rebooting. Please allow 60-90 seconds for the network to re-establish`
+      );
+    }
+  });
 });
