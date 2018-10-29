bfdd9e4fc7b57069b38e2a65ac12a2b7fa74903b
Justin <justscribner@gmail.com>
2018-10-29 13:36:50 -0500

    working on reset functionality

--- src/server/index.js
+++ src/server/index.js
@@ -38,9 +38,7 @@ app.get("/proxy/reset", function(req, res) {
         return;
       } else {
         console.log(`Connection: ${stdout}`);
-        res.send(
-          '<a href="http://ipfingerprints.com">Wait 15-30 seconds and click here to check your new ip address</a>'
-        );
+        res.end();
       }
     }
   );
