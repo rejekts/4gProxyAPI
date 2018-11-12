4f6ded406319a20e27e8f5ea1163f4c89ae63d20
Justin <justscribner@gmail.com>
2018-11-12 16:11:08 -0600

    working on errors in the hard reset

---
+++ src/server/clearAndHardReset.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Put this in the /usr/local/bin/ folder and chmod the file with sudo chmod +x 
+sudo systemctl stop squid
+echo "squid stopped"
+rm -Rf /var/spool/squid/
+echo "removed the cache dirs"
+mkdir /var/spool/squid
+echo "made the cache dir again"
+chown pi:pi /var/spool/squid
+echo "set chown for the new folder"
+squid -z
+echo "ran squid -z to setup thr cache again and calling sudo reboot next"
+sudo reboot
