01840761c04a41f12574e93f3912ad95348c8b3c
Justin <justscribner@gmail.com>
2018-11-19 16:23:04 -0600

    working on cloaking api on a new server

--- src/server/index.js
+++ src/server/index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const child_process = require("child_process");
+const cors = require("cors");
 
 const app = express();
 
