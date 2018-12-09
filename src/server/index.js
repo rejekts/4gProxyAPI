5f34ac8a3f6b78cc25f98709c721bac67f83975a
Justin <justscribner@gmail.com>
2018-12-08 21:52:42 -0600

    working on the personals endppoint for extension

--- src/server/index.js
+++ src/server/index.js
@@ -104,9 +104,9 @@ app.get("/proxy/reset/clear-cache", function(req, res) {
     });
 });
 
-app.post("/api/cookies", function(req, res) {
+app.post("/api/cookies/:cookies", function(req, res) {
   console.log("req.body => ", req.body);
-  console.log("req.params => ", req.params);
+  console.log("req.params => ", JSON.parse(JSON.stringify(req.params)));
   try {
     res.send("Got em!!");
   } catch (err) {
