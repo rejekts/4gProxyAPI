133b400f8855bfd224c0f9f41adb3d42e56a16e6
justscribner <justscribner@gmail.com>
2019-01-02 16:50:56 -0600

    working on dynamodb addition

--- src/server/routes/api/proxies.js
+++ src/server/routes/api/proxies.js
@@ -1,10 +1,10 @@
-const AWS = require('aws-sdk');
-const config = require('../../../config/config.js');
-const isDev = process.env.NODE_ENV !== 'production';
+const AWS = require("aws-sdk");
+const config = require("../../../config/config.js");
+const isDev = process.env.NODE_ENV !== "production";
 
-module.exports = (app) => {
+module.exports = app => {
   // Gets all fruits
-  app.get('/api/proxies', (req, res, next) => {
+  app.get("/api/proxies", (req, res, next) => {
     if (isDev) {
       AWS.config.update(config.aws_local_config);
     } else {
@@ -18,20 +18,20 @@ module.exports = (app) => {
       if (err) {
         res.send({
           success: false,
-          message: 'Error: Server error'
+          message: "Error: Server error"
         });
       } else {
         const { Items } = data;
         res.send({
           success: true,
-          message: 'Loaded fruits',
+          message: "Loaded fruits",
           fruits: Items
         });
       }
     });
   }); // end of app.get(/api/fruits)
   // Get a single fruit by id
-  app.get('/api/proxy', (req, res, next) => {
+  app.get("/api/proxy", (req, res, next) => {
     if (isDev) {
       AWS.config.update(config.aws_local_config);
     } else {
@@ -41,30 +41,30 @@ module.exports = (app) => {
     const docClient = new AWS.DynamoDB.DocumentClient();
     const params = {
       TableName: config.aws_table_name,
-      KeyConditionExpression: 'fruitId = :i',
+      KeyConditionExpression: "fruitId = :i",
       ExpressionAttributeValues: {
-        ':i': fruitId
+        ":i": fruitId
       }
     };
     docClient.query(params, function(err, data) {
       if (err) {
         res.send({
           success: false,
-          message: 'Error: Server error'
+          message: "Error: Server error"
         });
       } else {
-        console.log('data', data);
+        console.log("data", data);
         const { Items } = data;
         res.send({
           success: true,
-          message: 'Loaded fruits',
+          message: "Loaded fruits",
           fruits: Items
         });
       }
     });
   });
   // Add a fruit
-  app.post('/api/proxy', (req, res, next) => {
+  app.post("/api/proxy", (req, res, next) => {
     if (isDev) {
       AWS.config.update(config.aws_local_config);
     } else {
@@ -86,38 +86,17 @@ module.exports = (app) => {
       if (err) {
         res.send({
           success: false,
-          message: 'Error: Server error'
+          message: "Error: Server error"
         });
       } else {
-        console.log('data', data);
+        console.log("data", data);
         const { Items } = data;
         res.send({
           success: true,
-          message: 'Added fruit',
+          message: "Added fruit",
           fruitId: fruitId
         });
       }
     });
   });
 };
-
-
-alias gci='git add -A; git commit -m '
-alias gbr='git br'
-alias galiases='git aliases'
-alias gco='git co'
-alias ggo='git go'
-alias glist='git list'
-alias gph='git ph'
-alias gpl='git pl'
-alias gst='git st'
-alias gi='git i'
-alias grmc='git rmc'
-alias grmb='git rmb'
-alias ginmaster='git inmaster'
-alias gfth='git fth'
-alias gclear='git clear'
-alias gsth='git sth'
-alias gdf='git df'
-alias grs='git rs'
-alias gl='git l'
