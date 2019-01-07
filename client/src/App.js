fe4eaf7e478a95f97b1e2f7917e448b6b86fdc02
justscribner <justscribner@gmail.com>
2019-01-07 14:12:31 -0600

    got the react client side up and running with the servers

---
+++ client/src/App.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Axios from "axios";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+/*
+import React, { Component } from "react";
+import "./app.css";
+import ReactImage from "./react.png";
+import Axios from "axios";
+
+export default class App extends Component {
+  state = { currentIP: null };
+
+  componentDidMount() {
+    // const {
+    //   match: { params }
+    // } = this.props;
+    //`/proxy/reset/${params.uuid}`
+    Axios.get(`/proxy/list`)
+      .then(res => res.json())
+      .then(proxy => this.setState({ currentIP: proxy.browser_ip }));
+  }
+
+  // getUUID = function() {};
+
+  render() {
+    const { currentIP } = this.state;
+    return (
+      <div>
+        {currentIP ? (
+          <h1>{`Hello. Your current IP is: ${currentIP}. Please allow 3 - 5 minutes for the connection to reset with a new IP.`}</h1>
+        ) : (
+          <h1>Loading.. please wait!</h1>
+        )}
+        <div>
+          <button>Click here to refresh IP and see if its changed yet</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+*/
--- src/client/routes/routes.js
+++
@@ -1,7 +0,0 @@
-var appRouter = function(app) {
-  app.get("/", function(req, res) {
-    res.status(200).send("Welcome to our restful API");
-  });
-};
-
-module.exports = appRouter;
