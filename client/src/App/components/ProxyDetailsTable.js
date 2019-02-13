0e2b82a9f1b61c53d9dd070fd01627a3817b6230
justscribner <justscribner@gmail.com>
2019-02-13 16:36:58 -0600

    working on the external uisers view still

--- client/src/App/components/ProxyDetailsTable.js
+++ client/src/App/components/ProxyDetailsTable.js
@@ -8,20 +8,25 @@ class ProxyDetailsTable extends Component {
         <Table definition>
           <Table.Body>
             <Table.Row>
-              <Table.Cell width={10}>Proxy</Table.Cell>
-              <Table.Cell>{this.props.proxyIP}</Table.Cell>
-            </Table.Row>
-            <Table.Row>
               <Table.Cell>Current Browser IP</Table.Cell>
               <Table.Cell>{this.props.browserIP}</Table.Cell>
             </Table.Row>
             <Table.Row>
-              <Table.Cell>Old Browser IP</Table.Cell>
-              <Table.Cell>{this.props.oldBrowserIP}</Table.Cell>
+              <Table.Cell width={10}>Port</Table.Cell>
+              <Table.Cell>{this.props.port}</Table.Cell>
             </Table.Row>
+            <Table.Row>
+              <Table.Cell width={10}>Proxy IP</Table.Cell>
+              <Table.Cell>{this.props.proxyIP}</Table.Cell>
+            </Table.Row>
+
             <Table.Row>
               <Table.Cell>Proxy Reset Status</Table.Cell>
               <Table.Cell>{this.props.status}</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>Old Browser IP</Table.Cell>
+              <Table.Cell>{this.props.oldBrowserIP}</Table.Cell>
             </Table.Row>
           </Table.Body>
         </Table>
