911a60a232ca776e6be80176ca57f487c338be75
justscribner <justscribner@gmail.com>
2019-02-27 14:25:05 -0600

    working on dynamo model for user

--- serverA/index.js
+++ serverA/index.js
@@ -1,93 +1,82 @@
 //This is the API serverA file. It serves as the initiator of procedures and it communicates with the browser and send instructions to serverB to be executed
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const moment = require("moment-timezone");
-const childExec = require("child_process").exec;
-const util = require("util");
+const express = require('express');
+const bodyParser = require('body-parser');
+const moment = require('moment-timezone');
+const childExec = require('child_process').exec;
+const util = require('util');
 const exec = util.promisify(childExec);
-const path = require("path");
-const rp = require("request-promise");
+const path = require('path');
+const rp = require('request-promise');
+const passport = require('passport');
+
 const app = express();
 
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(express.static("dist"));
-app.enable("trust proxy");
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(express.static('dist'));
+app.enable('trust proxy');
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
-app.use(express.static(path.join(__dirname, "/../client/build")));
 
-const server = app.listen(10080, () =>
-  console.log("ServerA Listening on port 10080!")
-);
+app.use(express.static(path.join(__dirname, '/../client/build')));
+
+const PORT = process.env.PORT || 10080;
+app.listen(PORT, () => console.log('ServerA Listening on port 10080!'));
 // app.timeout = 360000;
 
-app.get("/", function(req, res) {
-  res.status(200).send("Nothing to see here.");
+app.get('/', function(req, res) {
+  res.status(200).send('Nothing to see here.');
 });
 
-app.get("/proxy/list", function(req, res) {
+app.get('/proxy/list', function(req, res) {
   console.log(
-    "/proxy/list API  Endpoint getting hit in serverA! Time => ",
-    moment().format("YYYY-MM-DDTHH:mm:ss")
+    '/proxy/list API  Endpoint getting hit in serverA! Time => ',
+    moment().format('YYYY-MM-DDTHH:mm:ss')
   );
 
   let options = {
-    host: "localhost",
-    port: "10090",
-    path: "/api/proxy/all",
-    url: "http://localhost:10090/api/proxy/all",
-    method: "GET",
+    host: 'localhost',
+    port: '10090',
+    path: '/api/proxy/all',
+    url: 'http://localhost:10090/api/proxy/all',
+    method: 'GET',
     headers: {
       // headers such as "Cookie" can be extracted from req object and sent to /test
     }
   };
   rp(options)
     .then(proxies => {
-      console.log("All Proxies in serverA => ", proxies);
+      console.log('All Proxies in serverA => ', proxies);
       res.status(200).send(proxies);
     })
     .catch(error => {
       if (error) {
-        console.log("error in /proxy/list => ", error);
+        console.log('error in /proxy/list => ', error);
       }
     });
 });
 
 //add new proxy to the db
-app.post("/proxy/add", function(req, res) {
-  const {
-    lanIP,
-    vpnIP,
-    proxyIP,
-    oldBrowserIP,
-    browserIP,
-    port,
-    carrier,
-    apn,
-    status
-  } = req.body;
+app.post('/proxy/add', function(req, res) {
+  const { lanIP, vpnIP, proxyIP, oldBrowserIP, browserIP, port, carrier, apn, status } = req.body;
 
   console.log(
-    "/proxy/add API  Endpoint getting hit in serverA! Time => ",
-    moment().format("YYYY-MM-DDTHH:mm:ss"),
-    " req.body => ",
+    '/proxy/add API  Endpoint getting hit in serverA! Time => ',
+    moment().format('YYYY-MM-DDTHH:mm:ss'),
+    ' req.body => ',
     req.body
   );
 
   let options = {
-    host: "localhost",
-    port: "10090",
-    path: "/api/proxy/add",
-    url: "http://localhost:10090/api/proxy/add",
-    method: "POST",
+    host: 'localhost',
+    port: '10090',
+    path: '/api/proxy/add',
+    url: 'http://localhost:10090/api/proxy/add',
+    method: 'POST',
     json: true,
     headers: {
       // headers such as "Cookie" can be extracted from req object and sent to /test
@@ -106,17 +95,17 @@ app.post("/proxy/add", function(req, res) {
   };
   rp(options)
     .then(proxies => {
-      console.log("Response to /proxy/add in serverA => ", proxies);
+      console.log('Response to /proxy/add in serverA => ', proxies);
       res.status(200).send(proxies);
     })
     .catch(error => {
       if (error) {
-        console.log("error in /proxy/list => ", error);
+        console.log('error in /proxy/list => ', error);
       }
     });
 });
 //grab IP from the db
-app.get("/proxy/getIPFromDb", function(req, res) {
+app.get('/proxy/getIPFromDb', function(req, res) {
   const proxyServerID = req.query.proxyServerID;
 
   // console.log(
@@ -127,11 +116,11 @@ app.get("/proxy/getIPFromDb", function(req, res) {
   // );
 
   let options1 = {
-    host: "localhost",
-    port: "10090",
-    path: "/api/proxy/browserIP",
-    url: "http://localhost:10090/api/proxy/browserIPFromDb",
-    method: "GET",
+    host: 'localhost',
+    port: '10090',
+    path: '/api/proxy/browserIP',
+    url: 'http://localhost:10090/api/proxy/browserIPFromDb',
+    method: 'GET',
     qs: {
       proxyServerID: proxyServerID
     }
@@ -143,12 +132,12 @@ app.get("/proxy/getIPFromDb", function(req, res) {
     })
     .catch(error => {
       if (error) {
-        console.log("error in /proxy/get_ip => ", error);
+        console.log('error in /proxy/get_ip => ', error);
       }
     });
 });
 //grab IP straight from the client machine and set in db
-app.get("/proxy/getIp", function(req, res) {
+app.get('/proxy/getIp', function(req, res) {
   const proxyServerID = req.query.proxyServerID;
 
   // console.log(
@@ -159,11 +148,11 @@ app.get("/proxy/getIp", function(req, res) {
   // );
 
   let options1 = {
-    host: "localhost",
-    port: "10090",
-    path: "/api/proxy/browserIP",
-    url: "http://localhost:10090/api/proxy/browserIP",
-    method: "GET",
+    host: 'localhost',
+    port: '10090',
+    path: '/api/proxy/browserIP',
+    url: 'http://localhost:10090/api/proxy/browserIP',
+    method: 'GET',
     qs: {
       proxyServerID: proxyServerID
     }
@@ -175,44 +164,44 @@ app.get("/proxy/getIp", function(req, res) {
     })
     .catch(error => {
       if (error) {
-        console.log("error in /proxy/get_ip => ", error);
+        console.log('error in /proxy/get_ip => ', error);
       }
     });
 });
 
-app.get("/proxy/reset", function(req, res) {
+app.get('/proxy/reset', function(req, res) {
   const proxyServerID = req.query.proxyServerID;
   let oldIP;
   let newIP;
 
   console.log(
-    "Reset API serverA Endpoint getting hit! Time => ",
-    moment().format("YYYY-MM-DDTHH:mm:ss"),
-    "params => ",
+    'Reset API serverA Endpoint getting hit! Time => ',
+    moment().format('YYYY-MM-DDTHH:mm:ss'),
+    'params => ',
     req.query
   );
 
   let options1 = {
-    host: "localhost",
-    port: "10090",
-    path: "/api/proxy",
-    url: "http://localhost:10090/api/proxy",
-    method: "GET",
+    host: 'localhost',
+    port: '10090',
+    path: '/api/proxy',
+    url: 'http://localhost:10090/api/proxy',
+    method: 'GET',
     qs: {
       proxyServerID: proxyServerID,
-      status: "Pending"
+      status: 'Pending'
     }
   };
   //send request to serverB to start reset procedures and update the status and grap current browserIP
   rp(options1)
     .then(prx => {
-      console.log("Proxy Data in serverA => ", prx);
+      console.log('Proxy Data in serverA => ', prx);
       let options2 = {
-        host: "localhost",
-        port: "10090",
-        path: "/api/proxy/reset",
-        url: "http://localhost:10090/api/proxy/reset",
-        method: "GET",
+        host: 'localhost',
+        port: '10090',
+        path: '/api/proxy/reset',
+        url: 'http://localhost:10090/api/proxy/reset',
+        method: 'GET',
         qs: {
           proxyServerID: proxyServerID,
           host: prx.lanIP,
@@ -223,14 +212,11 @@ app.get("/proxy/reset", function(req, res) {
       //send reset instructions to serverB
       rp(options2)
         .then(prz => {
-          console.log(
-            "Ran the reset endpoint in the serverA endpoint => ",
-            prz
-          );
+          console.log('Ran the reset endpoint in the serverA endpoint => ', prz);
         })
         .catch(error => {
           if (error) {
-            console.log("error in /proxy/reset 2nd call => ", error);
+            console.log('error in /proxy/reset 2nd call => ', error);
           }
         });
       res.status(200).send(prx);
@@ -239,22 +225,22 @@ app.get("/proxy/reset", function(req, res) {
     })
     .catch(error => {
       if (error) {
-        console.log("error in /proxy/reset 1st call => ", error);
+        console.log('error in /proxy/reset 1st call => ', error);
       }
     });
 });
 
-app.get("/proxy/reset/hard", function(req, res) {
-  const host = req.query["host"];
-  const network = req.query["network"];
-  const apn = req.query["apn"];
+app.get('/proxy/reset/hard', function(req, res) {
+  const host = req.query['host'];
+  const network = req.query['network'];
+  const apn = req.query['apn'];
 
   console.log(
-    "Hard Reset API Endpoint getting hit!",
-    "host => ",
+    'Hard Reset API Endpoint getting hit!',
+    'host => ',
     host,
-    "Time => ",
-    moment().format("YYYY-MM-DDTHH:mm:ss")
+    'Time => ',
+    moment().format('YYYY-MM-DDTHH:mm:ss')
   );
 
   exec(`ssh pi@${host} "sudo reboot"`)
@@ -265,21 +251,21 @@ app.get("/proxy/reset/hard", function(req, res) {
       console.log(`Connection to ${host}: ${data.stdout}`);
     })
     .catch(err => {
-      console.log("err in the catch => ", err);
+      console.log('err in the catch => ', err);
       res.send(
         `Proxy Server Rebooting. Please allow 60-90 seconds for the network to re-establish. You can close this browser tab now.`
       );
     });
 });
 
-app.get("/proxy/reset/clear-cache", function(req, res) {
-  const host = req.query["host"];
+app.get('/proxy/reset/clear-cache', function(req, res) {
+  const host = req.query['host'];
   console.log(
-    "Clear Cache API Endpoint getting hit!",
-    "host => ",
+    'Clear Cache API Endpoint getting hit!',
+    'host => ',
     host,
-    "Time => ",
-    moment().format("YYYY-MM-DDTHH:mm:ss")
+    'Time => ',
+    moment().format('YYYY-MM-DDTHH:mm:ss')
   );
 
   exec(`ssh pi@${host} "sudo /usr/local/bin/clearAndHardReset.sh"`)
@@ -290,31 +276,31 @@ app.get("/proxy/reset/clear-cache", function(req, res) {
       console.log(`Connection to ${host}: ${data.stdout}`);
     })
     .catch(err => {
-      console.log("err in the catch => ", err);
+      console.log('err in the catch => ', err);
     });
 });
 
-app.post("/api/cookies", function(req, res) {
-  console.log("req.body => ", req.body);
-  console.log("req.params => ", req.params);
+app.post('/api/cookies', function(req, res) {
+  console.log('req.body => ', req.body);
+  console.log('req.params => ', req.params);
 
   for (let i = 0; i < req.body.cookies.length; i++) {
     let cookie = req.body.cookies[i];
-    console.log(cookie.domain, " - ", cookie.name, " - ", cookie.value);
+    console.log(cookie.domain, ' - ', cookie.name, ' - ', cookie.value);
     console.log(
       req.body.changed.cookie.domain,
-      " - ",
+      ' - ',
       req.body.changed.cookie.name,
-      " - ",
+      ' - ',
       req.body.changed.cookie.value
     );
   }
   try {
-    res.send("got em!!");
+    res.send('got em!!');
   } catch (err) {
     if (err) {
       console.error(err);
-      res.send("Error in the /api/cookies post method");
+      res.send('Error in the /api/cookies post method');
     }
   }
 
@@ -367,25 +353,25 @@ app.post("/api/cookies", function(req, res) {
 });
 
 //Facebot Endpoints below
-app.get("/bot/reset", function(req, res) {
-  const host = req.query["host"];
-  const network = req.query["network"];
+app.get('/bot/reset', function(req, res) {
+  const host = req.query['host'];
+  const network = req.query['network'];
   let oldIP;
   let newIP;
 
   console.log(
-    "Reset API Endpoint getting hit!",
-    "host ip => ",
+    'Reset API Endpoint getting hit!',
+    'host ip => ',
     host,
-    " network => ",
+    ' network => ',
     network,
-    " Time => ",
-    moment().format("YYYY-MM-DDTHH:mm:ss")
+    ' Time => ',
+    moment().format('YYYY-MM-DDTHH:mm:ss')
   );
 
   grabClientIP(host)
     .then(ip => {
-      console.log("Return of oldIP in the /bot/reset endpoint => ", ip);
+      console.log('Return of oldIP in the /bot/reset endpoint => ', ip);
       if (!ip) {
         grabClientIP(host).then(ip2 => {
           if (!ip2) {
@@ -410,10 +396,10 @@ app.get("/bot/reset", function(req, res) {
       resetClientIPAddress(host, network, oldIP, (err, ip) => {
         if (err) {
           console.log(
-            "We have an error in the main /proxy/reset endpoint when calling the resetClientIpAddress method. err => ",
+            'We have an error in the main /proxy/reset endpoint when calling the resetClientIpAddress method. err => ',
             err
           );
-          if (err.stderr.indexOf("closed by remote host") >= 0) {
+          if (err.stderr.indexOf('closed by remote host') >= 0) {
             res
               .status(255)
               .send(
@@ -429,10 +415,7 @@ app.get("/bot/reset", function(req, res) {
                   );
               })
               .catch(err => {
-                console.log(
-                  "Error trying to reset the ip in the main endpoint. Error: ",
-                  err
-                );
+                console.log('Error trying to reset the ip in the main endpoint. Error: ', err);
                 res
                   .status(255)
                   .send(
@@ -441,7 +424,7 @@ app.get("/bot/reset", function(req, res) {
               });
           }
         } else {
-          console.log("Success!! newIP in the endpoint!! => ", ip);
+          console.log('Success!! newIP in the endpoint!! => ', ip);
           res
             .status(200)
             .send(
@@ -453,6 +436,6 @@ app.get("/bot/reset", function(req, res) {
 });
 
 //catchall to send all other traffic than endpoints above to the react app
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/../client/build/index.html"));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../client/build/index.html'));
 });
---
+++ serverB/dynamodb/proxyUser.js
@@ -0,0 +1,199 @@
+const uuidv1 = require('uuid/v4');
+
+const Joi = require('joi');
+const Dynamo = require('./dynamodb.js');
+const Logger = require('../log');
+
+const logger = new Logger({ context: 'DYNAMO DB' });
+
+class ProxyUser {
+  constructor(data, cb) {
+    this.dynamo = new Dynamo({
+      accessKeyId: data.accessKeyId,
+      secretAccessKey: data.secretAccessKey,
+      region: data.region
+    });
+
+    this.pr = this.dynamo.define('ProxyUser', {
+      hashKey: 'proxyUserID',
+      // add the timestamp attributes (updatedAt, createdAt)
+      timestamps: true,
+      schema: {
+        proxyUserID: this.dynamo.types.uuid(),
+        firstName: Joi.string(),
+        lastName: Joi.string(),
+        email: Joi.string().email({
+          minDomainAtoms: 2
+        }),
+        password: Joi.string(),
+        plans: Joi.object({
+          plan1NameNEEDEDHERE: Joi.object({
+            isActive: Joi.boolean(),
+            price: Joi.string(),
+            planID: Joi.string(),
+            expirationDate: Joi.string(),
+            paymentMethod: Joi.string()
+          }),
+          plan2NameNEEDEDHERE: Joi.object({
+            isActive: Joi.boolean(),
+            price: Joi.string(),
+            planID: Joi.string(),
+            expirationDate: Joi.string(),
+            paymentMethod: Joi.string()
+          }),
+          plan3NameNEEDEDHERE: Joi.object({
+            isActive: Joi.boolean(),
+            price: Joi.string(),
+            planID: Joi.string(),
+            expirationDate: Joi.string(),
+            paymentMethod: Joi.string()
+          })
+        })
+      },
+      indexes: [
+        {
+          hashKey: 'email',
+          name: 'email-index',
+          type: 'global'
+        },
+        {
+          hashKey: 'lastName',
+          name: 'lastName-index',
+          type: 'global'
+        }
+      ]
+    });
+
+    this.dynamo.createTables(function(err) {
+      if (err) {
+        logger.error('Error creating tables: ', JSON.stringify(err, null, 2));
+      } else {
+        cb(this);
+        logger.info('Tables have been created');
+      }
+    });
+    return this;
+  }
+
+  create(data) {
+    const proxyUserID = uuidv1();
+    return new Promise((resolve, reject) => {
+      this.pr.create(
+        {
+          proxyUserID,
+          lanIP: data.lanIP,
+          vpnIP: data.vpnIP,
+          proxyIP: data.proxyIP,
+          oldBrowserIP: data.oldBrowserIP,
+          browserIP: data.browserIP,
+          port: data.port,
+          carrier: data.carrier,
+          apn: data.apn,
+          status: data.status,
+          resetURL: `http://proxy2.confucius.marketing/reset/${proxyUserID}`
+        },
+        (err, res) => {
+          if (err) {
+            logger.error(err);
+            reject(err);
+          }
+          logger.info(`Proxy data was written in dynamoDB: ${JSON.stringify(res, 4, '')}`);
+          // console.log("Creating proxies in DynamoDb => ", res);
+          resolve(res);
+        }
+      );
+    });
+  }
+
+  // possibly change the ability to update anything but the browserIPs and the status
+  update(proxyUserID, data) {
+    // console.log("data in the update of the proxyUser class => ", data);
+    return new Promise((resolve, reject) => {
+      this.pr.update(
+        {
+          proxyUserID,
+          lanIP: data.lanIP,
+          vpnIP: data.vpnIP,
+          proxyIP: data.proxyIP,
+          oldBrowserIP: data.oldBrowserIP,
+          browserIP: data.browserIP,
+          port: data.port,
+          carrier: data.carrier,
+          apn: data.apn,
+          status: data.status,
+          resetURL: data.resetURL
+        },
+        (err, res) => {
+          if (err) {
+            logger.error(err);
+            reject(err);
+          }
+          logger.info('Proxies field was updated:', res);
+          resolve(res);
+        }
+      );
+    });
+  }
+
+  get(proxyUserID) {
+    return new Promise((resolve, reject) => {
+      this.pr.get(
+        proxyUserID,
+        {
+          ConsistentRead: true,
+          AttributesToGet: [
+            'proxyUserID',
+            'lanIP',
+            'vpnIP',
+            'proxyIP',
+            'oldBrowserIP',
+            'browserIP',
+            'port',
+            'carrier',
+            'apn',
+            'status',
+            'resetURL',
+            'createdAt',
+            'updatedAt'
+          ]
+        },
+        (err, res) => {
+          if (err) {
+            reject(err);
+          } else if (res !== null) {
+            resolve(res.toJSON());
+          }
+        }
+      );
+    });
+  }
+
+  query(idxName, idx) {
+    return new Promise((resolve, reject) => {
+      this.pr
+        .query(idx)
+        .usingIndex(`${idxName}-index`)
+        .exec((err, res) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(res.Items);
+          }
+        });
+    });
+  }
+
+  getAll() {
+    return new Promise((resolve, reject) => {
+      this.pr
+        .scan()
+        .loadAll()
+        .exec((err, res) => {
+          if (err) reject(err);
+          resolve(res);
+        });
+    });
+  }
+}
+
+module.exports = ProxyUser;
